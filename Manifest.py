3df6038621df73e1d10c4c1c01cb304541db192e
garcialasheras <garcialasheras@da83c25e-960b-41b0-8e88-fd9b310f3f16>
2014-08-20 20:39:14 +0000

    fixed Manifest naming issue

---
+++ Manifest.py
@@ -0,0 +1,9 @@
+files = [
+    "hdl/asyncart_delay.vhd", 
+    "hdl/asyncart_demo.vhd", 
+    "hdl/asyncart_demo_1d.vhd", 
+    "hdl/asyncart_reg.vhd", 
+    "hdl/asyncart_sink.vhd", 
+    "hdl/asyncart_source.vhd"
+]
+
--- hdl/Makefile.py
+++
@@ -1,2 +0,0 @@
-files = ["asyncart_delay.vhd", "asyncart_demo.vhd", "asyncart_demo_1d.vhd", "asyncart_reg.vhd", "asyncart_sink.vhd", "asyncart_source.vhd"]
-
